02541f51919760330ca447b0df6f4588c3b40b93
Kawsar Alam Tasin <117931018+TheKATasin@users.noreply.github.com>
2022-11-11 00:17:39 +0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("#q_one").click(function(){
+        $("#a_one").slideToggle();
+    });
+
+    $("#q_two").click(function(){
+        $("#a_two").slideToggle();
+    });
+
+    $("#q_three").click(function(){
+        $("#a_three").slideToggle();
+    });
+
+    $("#q_four").click(function(){
+        $("#a_four").slideToggle();
+    });
+
+    $("#q_five").click(function(){
+        $("#a_five").slideToggle();
+    });
+});
